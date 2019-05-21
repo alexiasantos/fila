12b34a76b9db2bba34c19ce867672c16a2fc19c1
nassim <nassimmoussa1993@gmail.com>
2019-05-20 21:35:40 -0300

    05 versao final com fila global

---
+++ Global 05.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct no {
+ int info;
+ struct no* ant;
+ struct no* prox;
+}fila;
+
+static fila* p;
+static fila* u;
+
+void criafila (void) {
+   p = (fila*) malloc (sizeof(fila));
+   u = (fila*) malloc (sizeof(fila));
+   p=NULL;
+   u=NULL;
+}
+
+
+void insere (int conteudo){
+	fila *novo;
+	novo = (fila*) malloc (sizeof(fila));
+	novo->info = conteudo;
+	novo->prox = u;
+	novo->ant = NULL;
+	if (p == NULL){
+		p=novo;
+		u=novo;
+	}else {
+		u->ant = novo;
+		u = novo;	
+	}
+}
+
+void remove () {
+ 	fila *aux = p;
+ 	p=p->ant;
+ 	
+ 	p->prox = NULL;
+ 	free (aux);
+}
+
+
+void mostraFila(){
+	printf("\nElementos da Fila\n");
+	fila* x = u;
+	while (x != NULL){
+      printf(" [%i]",x->info);
+      x = x->prox; 
+    }
+}
+
+
+int main (){
+	criafila();
+	insere(1);
+	insere(2);
+	insere(3);
+	insere(4);
+	mostraFila();
+	remove();
+	mostraFila();
+}
